72372566aac66a00c580d479000e63a17c0ac4ff
wrzdx <7zhhovalyg@mail.ru>
2025-07-02 14:20:52 +0300

    Complete frontend part

--- labs/lab02/frontend/lib/user_service.dart
+++ labs/lab02/frontend/lib/user_service.dart
@@ -1,8 +1,6 @@
 class UserService {
   Future<Map<String, String>> fetchUser() async {
-    // TODO: Simulate fetching user data for tests
-    // await Future.delayed(...)
-    // return {'name': ..., 'email': ...}
-    throw UnimplementedError();
+    await Future.delayed(Duration(milliseconds: 10));
+    return {'name': 'Alice', 'email': 'alice@example.com'};
   }
 }
