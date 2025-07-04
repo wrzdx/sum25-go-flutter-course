85d9cc83a6b36651eb7bdfb2c8473459093f38fb
wrzdx <7zhhovalyg@mail.ru>
2025-07-04 16:25:43 +0300

    Implemented chat application with REST API

--- labs/lab03/backend/main.go
+++ labs/lab03/backend/main.go
@@ -1,21 +1,31 @@
 package main
 
 import (
+	"lab03-backend/api"
+	"lab03-backend/storage"
 	"log"
+	"net/http"
+	"time"
 )
 
 func main() {
-	// TODO: Create a new memory storage instance
-	// TODO: Create a new API handler with the storage
-	// TODO: Setup routes using the handler
-	// TODO: Configure server with:
-	//   - Address: ":8080"
-	//   - Handler: the router
-	//   - ReadTimeout: 15 seconds
-	//   - WriteTimeout: 15 seconds
-	//   - IdleTimeout: 60 seconds
-	// TODO: Add logging to show server is starting
-	// TODO: Start the server and handle any errors
+	store := storage.NewMemoryStorage()
 
-	log.Println("TODO: Implement main function")
+	handler := api.NewHandler(store)
+
+	router := handler.SetupRoutes()
+
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
+	log.Println("Starting server on :8080")
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
